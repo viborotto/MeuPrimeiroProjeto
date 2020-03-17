11afa1f89943155a81af94f9d749af043825a434
viborotto <vittoriaborotto@gmail.com>
2020-03-17 20:12:01 -0300

    Arrays and Dicionaries

--- Arrays&Dicionaries&Sets.playground/Contents.swift
+++ Arrays&Dicionaries&Sets.playground/Contents.swift
@@ -27,3 +27,38 @@ print(array)
 
 array.sort()
 print(array)//array foi ordenado
+
+
+//Dicionarios : chave e valor, de qualquer tipo
+
+var dicionario: [String: Any] = ["chave 1": "Valor 1", "chave 2": "valor 2"]
+
+print(dicionario)
+
+dicionario["chave 1"]
+
+var dicionario2: [String: Int] = ["chave1": 1, "chave2": 2]
+dicionario2["chave1"]
+
+//adicionar
+dicionario["chave3"] = "valor 3"
+print(dicionario)
+//ou tambem
+
+dicionario.updateValue("valor 4", forKey: "chave4")
+print(dicionario)
+
+//alterar o valor da chave
+dicionario["chave3"] = "novo valor 3"
+//ou igual o de cima
+
+//remover
+dicionario.removeValue(forKey: "chave 1")
+print(dicionario)
+dicionario.remove(at: dicionario.index(forKey: "chave 2"))!
+print(dicionario)
+
+
+
+
+//Sets
